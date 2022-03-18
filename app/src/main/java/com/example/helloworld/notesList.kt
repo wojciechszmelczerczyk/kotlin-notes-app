0569ee31a96f5e7fcfc09f288439ca1dff209c2b
Wojciech <szmelczerczykwojciech@gmail.com>
2022-03-18 16:04:10 +0100

    start with fingerprint auth

---
+++ app/src/main/java/com/example/helloworld/notesList.kt
@@ -0,0 +1,11 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class notesList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes_list)
+    }
+}
