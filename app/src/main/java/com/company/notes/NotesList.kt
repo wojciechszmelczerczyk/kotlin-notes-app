f9e300b5c2e874f3dc4b5d95e83123e82e2be9db
Wojciech <szmelczerczykwojciech@gmail.com>
2022-03-24 16:43:04 +0100

    recycler and adapter for displaying dynamic list added

--- app/src/main/java/com/company/notes/NotesList.kt
+++ app/src/main/java/com/company/notes/NotesList.kt
@@ -2,10 +2,26 @@ package com.company.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class NotesList : AppCompatActivity() {
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
     override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes_list)
+
+        layoutManager = GridLayoutManager(this, 2)
+
+        // intercept recycler view
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView.layoutManager = layoutManager
+
+        adapter = RecyclerAdapter()
+        recyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/company/notes/RecyclerAdapter.kt
@@ -0,0 +1,4 @@
+package com.company.notes
+
+class RecyclerAdapter {
+}
