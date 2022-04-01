814a1d024fea1c632ae7e1c33fe04efc8dacd55f
Wojciech <szmelczerczykwojciech@gmail.com>
2022-04-01 21:05:43 +0200

    remove unused code

--- app/src/main/java/com/company/notes/NotesList.kt
+++ app/src/main/java/com/company/notes/NotesList.kt
@@ -2,6 +2,7 @@ package com.company.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
@@ -15,9 +16,6 @@ class NotesList : AppCompatActivity() {
     private lateinit var dbref: DatabaseReference
     private lateinit var noteRecyclerView: RecyclerView
     private lateinit var noteArrayList: ArrayList<Note>
-
-//    private var layoutManager: RecyclerView.LayoutManager? = null
-//    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
     override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
@@ -42,7 +40,7 @@ class NotesList : AppCompatActivity() {
                 if(snapshot.exists()){
                     for(noteSnapshot in snapshot.children){
                         val note = noteSnapshot.getValue(Note::class.java)
-                        noteArrayList.add(note!!)
+                         noteArrayList.add(note!!)
                     }
                     noteRecyclerView.adapter = RecyclerAdapter(noteArrayList)
                 }
