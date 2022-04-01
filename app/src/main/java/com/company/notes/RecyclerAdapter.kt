598e73df72c586c4bc7eb605268fff3c3184aae0
Wojciech <szmelczerczykwojciech@gmail.com>
2022-04-01 19:32:04 +0200

    changes in Recycler in order to intercept firebase data

--- app/src/main/java/com/company/notes/RecyclerAdapter.kt
+++ app/src/main/java/com/company/notes/RecyclerAdapter.kt
@@ -4,13 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
-class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter(private val noteList: ArrayList<Note>)  : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    // for now arbitrary array
-    private var titles = arrayOf("Note 1", "Note 2", "Note 3", "Note 4", "Note 5", "Note 6", "Note 7", "Note 8", "Note 9", "Note 10", "Note 11", "Note 12", "Note 13", "Note 14", "Note 15", "Note 16", "Note 17", "Note 18", "Note 19", "Note 20", "Note 21", "Note 22", "Note 23", "Note 24", "Note 25", "Note 26", "Note 27")
+    private var notes: Array<String> = arrayOf("s","s","s","s","s","s","s","s")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.note_layout, parent, false)
@@ -19,24 +18,15 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
         // populate note layouts with note title
-        holder.noteTitle.text=titles[position]
+        val currentItem = noteList[position]
+        holder.noteTitle.text = currentItem.name
     }
 
     override fun getItemCount(): Int {
-        return titles.size
+        return noteList.size
     }
 
     inner class ViewHolder(noteView: View): RecyclerView.ViewHolder(noteView){
-        var noteTitle: TextView
-
-        init {
-            noteTitle = noteView.findViewById(R.id.note_title)
-            noteTitle.setOnClickListener{
-                val position: Int = absoluteAdapterPosition
-                Toast.makeText(noteView.context, "you clicked on ${titles[position]}", Toast.LENGTH_LONG).show()
-            }
-
-
-        }
+        var noteTitle: TextView = itemView.findViewById(R.id.note_title)
     }
 }
